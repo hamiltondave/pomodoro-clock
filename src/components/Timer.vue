847f969223fcaba8e1479c25b62b310619e17bfc
David Hamilton <davidhamilton17@gmail.com>
2021-01-12 20:12:09 -0700

    Remove boilerplate

---
+++ src/components/Timer.vue
@@ -0,0 +1,15 @@
+<template>
+  <p>25:00</p>
+</template>
+
+<script>
+export default {
+  name: 'Timer',
+  props: {
+    minutes: {
+      type: Number,
+      required: true,
+    },
+  }
+}
+</script>
