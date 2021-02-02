e829bc4601b9637a0e260e0f91af8e1557ff58bd
David Hamilton <davidhamilton17@gmail.com>
2021-02-02 10:28:19 -0700

    Add video background support

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,24 @@
 <template>
   <div id="app">
-    <main>
+    <video ref="videoRef" muted loop id="bg-video" />
+    <main id="content">
       <h1>pomodoro</h1>
       <TimerOptions v-model.number="selectedTimer" />
       <Timer :limit="timeLimits[selectedTimer]" />
+      <Settings @update-background="setBackground" />
     </main>
   </div>
 </template>
 
 <script>
+import Settings from '@/components/Settings'
 import TimerOptions from '@/components/TimerOptions'
 import Timer from '@/components/Timer'
 
 export default {
   name: 'App',
   components: {
+    Settings,
     TimerOptions,
     Timer,
   },
@@ -28,7 +32,17 @@ export default {
         short: 5,
       },
     }
-  }
+  },
+
+  methods: {
+    setBackground(file) {
+      const URL = window.URL
+      const fileUrl = URL.createObjectURL(file)
+      console.log(fileUrl)
+      this.$refs.videoRef.src = fileUrl
+      this.$refs.videoRef.play()
+    },
+  },
 }
 </script>
 
@@ -36,6 +50,31 @@ export default {
 @import '~@/styles/main.scss';
 
 #app {
+  background-color: $bg-dark;
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100vw;
+  height: 100vh;
+}
+
+#content {
+  position: absolute;
   text-align: center;
+  background: rgba(0, 0, 0, 0.5);
+  color: #f1f1f1;
+  max-width: 32rem;
+  padding: 1rem;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: 0;
+}
+
+#bg-video {
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: 0;
+  height: 100%;
 }
 </style>
---
+++ src/components/Settings.vue
@@ -0,0 +1,63 @@
+<template>
+  <form class="settings">
+    <label class="button file">
+      Set Background
+      <input
+        class="file__input"
+        type="file"
+        accept="video/*"
+        @change="updateBackground"
+      />
+    </label>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'Settings',
+
+  methods: {
+    updateBackground(event) {
+      const fileList = event.target.files
+      console.log(fileList)
+      if (fileList.length === 1) {
+        this.$emit('update-background', fileList[0])
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import '~@/styles/_variables.scss';
+
+.settings {
+  display: block;
+  margin-top: 1rem;
+}
+
+.button {
+  display: inline-block;
+  background-color: $bg-light;
+  border-radius: 4px;
+  color: $bg-dark;
+  padding: 1rem 1.5rem;
+  cursor: pointer;
+}
+
+.file {
+  position: relative;
+
+  &__input {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    opacity: 0;
+    outline: none;
+    cursor: pointer;
+    z-index: -1;
+  }
+}
+</style>
