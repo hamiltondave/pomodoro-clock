2faef54a1b6f03963338cfd26061ddd83efca2a5
David Hamilton <davidhamilton17@gmail.com>
2021-09-13 11:42:24 -0700

    Fix settings menu tests

--- tests/unit/components/Settings.spec.js
+++ tests/unit/components/Settings.spec.js
@@ -6,17 +6,11 @@ test('renders file input for background', () => {
   expect(wrapper.find('input[type="file"]').exists()).toBe(true)
 });
 
-test('emits update-background event on input change', () => {
+test('emits update-background event on input change', async () => {
   const wrapper = mount(Settings)
-  wrapper.vm.updateBackground({target: {files: [{}]}})
+  await wrapper.setData({ file: {} })
   expect(wrapper.emitted()['update-background']).toBeTruthy()
   expect(wrapper.emitted()['update-background'].length).toBe(1)
-})
-
-test('does not emit update-background event for empty file list', () => {
-  const wrapper = mount(Settings)
-  wrapper.vm.updateBackground({target: {files: []}})
-  expect(wrapper.emitted()['update-background']).not.toBeTruthy()
 })
 
 test('renders select for menu placement', () => {
@@ -24,8 +18,8 @@ test('renders select for menu placement', () => {
   expect(wrapper.find('select').exists()).toBe(true)
 })
 
-test('emits update-placement event on select change', () => {
+test('emits update-placement event on select change', async () => {
   const wrapper = mount(Settings)
-  wrapper.vm.updatePlacement({target: {value: 'bottom-right'}})
+  await wrapper.setData({ placement: 'bottom-right' })
   expect(wrapper.emitted()['update-placement']).toBeTruthy()
 })
